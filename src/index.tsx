88e62fa93be506f8773a00fdd86f27e128d9026d
tomppk <tomi.kivioja@gmail.com>
2021-04-07 20:32:02 +0300

    create index.tsx and initial App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Describes all the props and their structure that you expect
+// to pass as into your component
+interface AppProps {
+  color?: string;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Define App component.
+// Reference AppProps interface as React.Component generic type
+// This way Typescript knows the props that are going to be passed
+// to component
+class App extends React.Component<AppProps> {
+  // Define state property as object
+  state = { counter: 0 };
+
+  // Define methods as arrow functions to bind 'this' to
+  // this instance of App class
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
