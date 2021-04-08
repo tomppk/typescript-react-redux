d5b7c1f1b2721035361aa193660687943aaa4577
tomppk <tomi.kivioja@gmail.com>
2021-04-08 11:27:44 +0300

    add deleteTodo to todosReducer

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,3 +1,5 @@
+import { FetchTodosAction, DeleteTodoAction } from './todos';
+
 // Define action types we expect to use inside single
 // enum file.
 // Action type does not need to be string, just needs
@@ -8,3 +10,8 @@ export enum ActionTypes {
   fetchTodos,
   deleteTodo,
 }
+
+// Create a new type Action that is a type union of our
+// possible different actions
+// So can be either fetch or delete action
+export type Action = FetchTodosAction | DeleteTodoAction;
--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,15 +1,23 @@
-import { Todo, FetchTodosAction, ActionTypes } from '../actions';
+import { Todo, Action, ActionTypes } from '../actions';
 
 // First argument the type of state with default value of empty array
 // Second argument the dispatched action from action creator
+// Use type Action to contain all our different types of
+// actions
 // Use switch statement to cover different cases of actions the reducer
 // might receive.
 // If action type is ActionTypes.fetchTodos then return that actions payload
+// If type is ActionTypes.deleteTodo we return a new state array of Todos
+// by filtering our previous state object or array of Todos and for each Todo
+// if the todo.id is not equal to id in action.payload that todo is added to
+// new state. If the id is the one we want to delete then that todo is excluded
 // If no match then return previous state by default
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
